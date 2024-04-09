d09004f66cbd0ac93b1190ec177a919f17641a1c
André <andregparada@gmail.com>
2024-04-09 14:14:35 -0300

    pronto criar e listar

---
+++ src/routes.js
@@ -0,0 +1,70 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database
+
+export const routes = [
+    {
+        method: 'GET',
+        path: buildRoutePath('/tasks'),
+        handler: (req, res) => {
+            const { search } = req.query
+
+            const tasks = database.select('tasks', search ? {
+                title: search,
+                description: search
+            } : null)
+
+            return res.end(JSON.stringify(tasks))
+        }
+    },
+    {
+        method: 'POST',
+        path: buildRoutePath('/tasks'),
+        handler: (req, res) => {
+            const { title, description } = req.body
+            const date = new Date()
+
+            const task = {
+                id: randomUUID(),
+                title,
+                description,
+                completed_at: null,
+                created_at: date,
+                updated_at: date
+            }
+
+            database.insert('tasks', task)
+
+            return res.writeHead(201).end()
+        }
+    },
+    {
+        method: 'PUT',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+            const { title, description } = req.body
+            const date = new Date()
+
+            database.update('users', id, {
+                title,
+                description
+            })
+
+            return res.writeHead(204).end()
+        },
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+
+            database.delete('users', id)
+
+            return res.writeHead(204).end()
+        },
+    }
+]
